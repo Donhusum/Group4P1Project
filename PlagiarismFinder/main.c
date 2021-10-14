f10757372e8f007b8a14ebee0fd27471560d35a2
Donhusum <stefanlarsen@live.dk>
2021-10-14 14:07:16 +0200

    First try, a word checker

--- PlagiarismFinder/main.c
+++ PlagiarismFinder/main.c
@@ -1,6 +1,52 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+    char StringMan1[30], StringMan2[30];
+    long count = 0, sameWord = 0;
+    int a;
+
+    FILE *fileHandler1;
+    FILE *fileHandler2;
+
+    if ((fileHandler1 = fopen(
+            "C:\\Users\\stefa\\Google Drive\\AAU\\1. semester\\P1\\GIT\\Group4P1Project\\PlagiarismFinder\\HandInFromStudent.txt",
+            "r")) == NULL) {
+        printf("Error, no file ");
+    }
+
+
+    while ((a=fscanf(fileHandler1, "%s", StringMan1)) != -1) {
+        //printf("%d \n", a);
+        if ((fileHandler2 = fopen(
+                "C:\\Users\\stefa\\Google Drive\\AAU\\1. semester\\P1\\GIT\\Group4P1Project\\PlagiarismFinder\\OriginalDoc.txt",
+                "r")) == NULL) {
+            printf("Error, no file ");
+        }
+        while (fscanf(fileHandler2, "%s", StringMan2) != -1) {
+            if (strcmp(StringMan2, StringMan1) == 0) {
+                printf("%s, are in both docs\n");
+                sameWord++;
+            }
+            //printf("%d %s \n", count, StringMan1);
+            //printf("%d %s \n", count, StringMan2);
+        }
+        count++;
+        fclose(fileHandler2);
+
+    }
+    printf("Words in the tested document %d\n", count);
+    printf("Words identical %d\n", sameWord);
+
+
+/*
+    if (strcmp(StringMan2, StringMan1)==0)
+        printf("2 and 1, They are the same\n");
+    else
+        printf("2 and 1, They are not the same\n");
+*/
+
+    fclose(fileHandler1);
     return 0;
 }
