ee957c48a24bb89adc0d2434a0d19f1e2b2b47a6
Donhusum <stefanlarsen@live.dk>
2021-11-11 15:04:13 +0100

    Fixed the counter for sentence duplicates

--- PlagiarismFinder/main.c
+++ PlagiarismFinder/main.c
@@ -8,7 +8,9 @@ int stringToNumber(char string[]);
 
 int fourFirstStrings(char a[], char b[], char c[], char d[]);
 
-void numberFile(FILE *fileHandler1);
+void numberFile(FILE *fileHandler1, char location[]);
+
+void numberAccumulator(char location[], int numberChunk);
 
 void periodeRemover(char *dotString);
 
@@ -19,7 +21,6 @@ struct array {
 int main() {
 
     char StringMan1[30], StringMan2[30];  //Two Char arrays (strings) with capacity to contain 30 chars (a 30 letter string)
-    long count = 0, sameWord = 0; //Long is just an int that can contain bigger numbers
 
     FILE *fileHandler1;
     FILE *fileHandler2; // Two pointers to handle our files
@@ -36,21 +37,21 @@ int main() {
 
 // Just a tester
     fileHandler1 = fopen("OriginalDocTester.txt", "r");
-    numberFile(fileHandler1);
+    numberFile(fileHandler1, "NumberFileOrigin.txt");
+    fclose(fileHandler1);
+
+    fileHandler1 = fopen("HandInFromStudentTester.txt", "r");
+    numberFile(fileHandler1, "NumberFileTester.txt");
     fclose(fileHandler1);
 
 
     int chunkInt1, chunkInt2, chunkInt3;
-    fileHandler1 = fopen("NumberFileOrigin.txt", "r");
+    fileHandler1 = fopen("NumberFileOrigin.txt", "r+");
+
     while (fscanf(fileHandler1, " %d %d", &chunkInt1, &chunkInt2) != EOF) {
         printf(" %d %d\n", chunkInt1, chunkInt2);
     }
-    //chunkInt3 = getchar();
-    //putchar(chunkInt3);
-
-
-    //Important!!!
-    // If the printer pointer point right before the change and just make 1 change, wil that work?
+    fclose(fileHandler1);
 
     return 0;
 }
@@ -83,7 +84,7 @@ int stringToNumber(char string[]) {
 
 // Generates the chunk number
 int fourFirstStrings(char a[], char b[], char c[], char d[]) {
-    int i, numb = 0;
+    int numb = 0;
     numb = numb + 1000 * stringToNumber(a);
     numb = numb + 100 * stringToNumber(b);
     numb = numb + 10 * stringToNumber(c);
@@ -92,18 +93,18 @@ int fourFirstStrings(char a[], char b[], char c[], char d[]) {
 }
 
 // converts text to number chunks and puts them into a file
-void numberFile(FILE *fileHandler1) {
+void numberFile(FILE *fileHandler1, char location[]) {
     FILE *fileHandler2;
     char stringHandler1[40], stringHandler2[40], stringHandler3[40], stringHandler4[40], dotFinder, stringMult[4][40];
     struct array stringObject1;
-    int numberChunk, duplicateChecker, dubVal, endDot, scanSucces, dubCount;
-    fileHandler2 = fopen("NumberFileOrigin.txt", "w+");
+    int numberChunk, endDot;
+    fileHandler2 = fopen(location, "w+");
     while (fscanf(fileHandler1, " %s %s %s %s", stringHandler1, stringHandler2, stringHandler3, stringHandler4) > -1) {
         int len, i, j;
 
         //Fjerner punktummer til sidst
 
-        //periodeRemover(stringHandler1);
+        //periodeRemover(stringHandler1); Do not currently work!
         len = strlen(stringHandler1);
         for (i = 0; i < len; i++) {
             if (stringHandler1[i] == '.') {
@@ -144,19 +145,7 @@ void numberFile(FILE *fileHandler1) {
         printf(" %s %s %s %s\n", stringHandler1, stringHandler2, stringHandler3, stringHandler4);
         numberChunk = fourFirstStrings(stringHandler1, stringHandler2, stringHandler3, stringHandler4);
 
-        //printf("Scanned elements: %d\n", scanSucces = fscanf(fileHandler2, " %d %d", &duplicateChecker, &dubVal));
-        rewind(fileHandler2);
-        while ((scanSucces = fscanf(fileHandler2, " %d %d", &duplicateChecker, &dubVal)) > -1) {
-            if (numberChunk == duplicateChecker) {
-                dubCount = dubVal + 1;
-                printf("DUBVAL!\n");
-            }
-            //printf("Duplicate checker = %d dubcount = %d scanSucces = %d\n", duplicateChecker, dubCount, scanSucces);
-        }
-
-
-        //Skriver hashet i filen 
-        fprintf(fileHandler2, " %d %d\n", numberChunk, 1);
+        numberAccumulator(location, numberChunk);
 
 
         //Flytter dotten, medmindre der findes et slutpunktum
@@ -173,6 +162,31 @@ void numberFile(FILE *fileHandler1) {
 }
 
 
+void numberAccumulator(char location[], int numberChunk){
+    FILE *fileHandler3;
+    int dubVal = 0, duplicateChecker;
+    fileHandler3 = fopen(location, "r+");
+    while (fscanf(fileHandler3, " %d", &duplicateChecker) != EOF) {
+        if (duplicateChecker == numberChunk) {
+            fscanf(fileHandler3, " %d", &dubVal);
+            fseek(fileHandler3, -1, SEEK_CUR);
+            fprintf(fileHandler3, " %d", (dubVal+1));
+            fseek(fileHandler3, 0, SEEK_CUR);
+            printf(" Number %d with dublicate numb %d, one up.\n", numberChunk, dubVal);
+            break;
+        } else {
+            fscanf(fileHandler3, " %d", &duplicateChecker);
+        }
+        dubVal = 0;
+    }
+    if (dubVal == 0) {
+        fprintf(fileHandler3, " %d %d\n", numberChunk, 1);
+    }
+
+    fclose(fileHandler3);
+}
+
+// Do currently not work
 void periodeRemover(char *dotString) {
     int len, i, j;
     len = strlen(dotString);
@@ -186,6 +200,8 @@ void periodeRemover(char *dotString) {
     }
     printf(" %c %c %c %c %c %c\n", dotString[0], dotString[1], dotString[2], dotString[3], dotString[4], dotString[5]);
 }
+
+
 
 
 
