82426e8e19ba72162a949b78a8301b2d705780d9
Christoffer Vagn Lorenzen <christoffer.lorenzen@outlook.com>
2021-11-11 10:31:08 +0100

    Dubval works somewhat

--- PlagiarismFinder/main.c
+++ PlagiarismFinder/main.c
@@ -127,7 +127,7 @@ int fourFirstStrings(char a[], char b[], char c[], char d[]) {
 void numberFile(FILE *fileHandler1) {
     FILE *fileHandler2;
     char stringHandler1[40], stringHandler2[40], stringHandler3[40], stringHandler4[40], dotFinder;
-    int numberChunk, duplicateChecker, dubVal, endDot, scanSucces, dubCount;
+    int numberChunk, duplicateChecker, endDot, scanSucces, dubVal;
     fileHandler2 = fopen("NumberFileOrigin.txt", "w+"); //Fejl i mode, vil ikke scanne
     while (fscanf(fileHandler1, " %s %s %s %s", stringHandler1, stringHandler2, stringHandler3, stringHandler4) > -1) {
         int len, i, j;
@@ -176,18 +176,24 @@ void numberFile(FILE *fileHandler1) {
 
         //printf("Scanned elements: %d\n", scanSucces = fscanf(fileHandler2, " %d %d", &duplicateChecker, &dubVal));
         rewind(fileHandler2);
+        dubVal = 1;
+        int dubTrue = 0;
         while ((scanSucces = fscanf(fileHandler2, " %d %d", &duplicateChecker, &dubVal)) > -1) {
             if (numberChunk == duplicateChecker) {
-                dubCount = dubVal + 1;
-                printf("DUBVAL!\n");
+                dubVal ++;
+                printf("DUBVAL! = %d\n", dubVal);
+                dubTrue = dubVal;
             }
             //printf("Duplicate checker = %d dubcount = %d scanSucces = %d\n", duplicateChecker, dubCount, scanSucces);
         }
-
          
         //Skriver hashet i filen 
-        fprintf(fileHandler2, " %d %d\n", numberChunk, 1);
-
+        if(dubTrue){
+            fprintf(fileHandler2, " %d %d\n", numberChunk, dubTrue);
+        }
+        else{
+            fprintf(fileHandler2, " %d %d\n", numberChunk, 1);
+        }
         
         //Flytter dotten, medmindre der findes et slutpunktum
         if (!endDot){
